5e5a637ff335ccc928a78889f4c861c199eb63be
Gabriel Schwandl <g.schwandl@students.htl-leonding.ac.at>
2025-03-24 07:42:46 +0100

    added files and nav

--- project/script.js
+++ project/script.js
@@ -1,3 +1,14 @@
+let lvls = document.getElementById('levels');
+
+function showAllLevels() {
+    let string = '';
+
+    for(let i = 0; i < 6; i++) {
+        string = `
+        <a><img src=''></a>
+        `
+    }
+}
 function goingDark() {
-    
+
 }
