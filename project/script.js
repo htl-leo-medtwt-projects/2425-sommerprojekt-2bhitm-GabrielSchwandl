82526ea95284312a45355ac7d88bf0ba5159b09c
Gabriel Schwandl <g.schwandl@students.htl-leonding.ac.at>
2025-03-28 10:51:45 +0100

    added styling to levels

--- project/script.js
+++ project/script.js
@@ -3,7 +3,7 @@ let lvls = document.getElementById('levels');
 function showAllLevels() {
     let string = '';
 
-    for(let i = 0; i < 6; i++) {
+    for(let i = 1; i < 7; i++) {
         string += `
         <a href="./game.html"><img class="pics" src="./img/pic${i}.jpg"></a>
         `
